27207bd3ec5ada2b3baeb1853ae14014924787ec
Shawn Presser <shawnpresser@gmail.com>
2022-09-29 18:26:16 -0500

    bel: Add some tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,16 @@
 import unittest
 
-import yel
+from yel import bel
 
 class TestCase(unittest.TestCase):
-  def test_basic(self):
-    self.assertEqual(1, 1)
+    def test_basic(self):
+        self.assertEqual(1, 1)
+    def test_bel(self):
+        test = self.assertEqual
+        test(1, bel.bel(1))
+        test(1, bel.bel(["let", "x", 1, "x"]))
+        x = 1
+        test(1, bel.bel("x", bel.unset, [locals()]))
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
